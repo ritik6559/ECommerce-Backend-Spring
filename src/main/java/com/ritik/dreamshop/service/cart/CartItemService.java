6e52cc6dd81e06f132783412bcbde0276cdc2548
Ritik6559 <ritikjoshi741@gmail.com>
2025-01-07 12:13:11 +0530

    cart and cart item services implemented

--- src/main/java/com/ritik/dreamshop/service/cart/CartItemService.java
+++ src/main/java/com/ritik/dreamshop/service/cart/CartItemService.java
@@ -26,7 +26,9 @@ public class CartItemService implements ICartItemService{
     public void addItemToCart(Long cartId, Long productId, int quantity) {
         Cart cart = cartService.getCart(cartId);
         Product product = productService.getProductById(productId);
-        CartItem cartItem = cart.getItems().stream().filter(item -> item.getProduct().getId().equals(productId))
+        CartItem cartItem = cart.getItems()
+                .stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
                 .findFirst().orElse(new CartItem());
         if(cartItem.getId() == null){
             cartItem.setCart(cart);
@@ -40,16 +42,40 @@ public class CartItemService implements ICartItemService{
         cart.addItem(cartItem);
         cartItemRepository.save(cartItem);
         cartRepository.save(cart);
-
     }
 
     @Override
     public void removeItemFromCart(Long cartId, Long productId) {
-
+        Cart cart = cartService.getCart(cartId);
+        CartItem itemToRemove = getCartItem(cartId, productId);
+        cart.removeItem(itemToRemove);
+        cartRepository.save(cart);
     }
 
     @Override
     public void updateItemQuantity(Long cartId, Long productId, int quantity) {
+        Cart cart = cartService.getCart(cartId);
+        cart.getItems()
+                .stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst()
+                .ifPresent(item -> {
+                    item.setQuantity(quantity);
+                    item.setUnitPrice(item.getProduct().getPrice());
+                    item.setTotalPrice();
+                });
 
+        BigDecimal totalAmount = cart.getTotalAmount();
+        cart.setTotalAmount(totalAmount);
+        cartRepository.save(cart);
+    }
+
+    @Override
+    public CartItem getCartItem(Long cartId, Long productId){
+        Cart cart = cartService.getCart(cartId);
+        return cart.getItems()
+                .stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst().orElseThrow(() -> new ResourceNotFoundException("Product not found"));
     }
 }
--- src/main/java/com/ritik/dreamshop/service/cart/ICartItemService.java
+++ src/main/java/com/ritik/dreamshop/service/cart/ICartItemService.java
@@ -1,8 +1,12 @@
 package com.ritik.dreamshop.service.cart;
 
 
+import com.ritik.dreamshop.model.cart.CartItem;
+
 public interface ICartItemService {
     void addItemToCart(Long cartId, Long productId, int quantity);
     void removeItemFromCart(Long cartId, Long productId);
     void updateItemQuantity(Long cartId, Long productId, int quantity);
+
+    CartItem getCartItem(Long cartId, Long productId);
 }
